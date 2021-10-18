a6f5833fca191cf54d45a9d2ad15b8606a0d21ce
Zhouyao Xie <zx2226@columbia.edu>
2021-10-18 12:02:55 -0400

    Update requests_demo.py

--- HTTP/requests_demo.py
+++ HTTP/requests_demo.py
@@ -1,6 +1,6 @@
 import requests
 
-# # demo 1: GET with requests.get()
+# GET
 url = 'http://api.lyrics.ovh/v1/Ed Sheeran/Thinking out Loud'
 response = requests.get(url)
 if response.status_code == 404:
@@ -8,12 +8,17 @@ if response.status_code == 404:
 if response.status_code == 200:
 	print(response.text)
 
-# demo 2: POST with requests.post()
+# POST
 import json 
-
 url = 'https://sentim-api.herokuapp.com/api/v1/'
-text = 'this is a happy post!'
-headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
-response = requests.post(url, headers = headers, data = json.dumps({'text': text}))
+headers = {'Content-Type': 'application/json'}
+new_data = {'text': 'this is a happy post!'}
+response = requests.post(url, headers = headers, data = json.dumps(new_data))
 print(response.status_code)
-print(json.loads(response.text)['result'])
+print(json.loads(response.text)) # or, response.json()
+
+# PUT PATCH
+# no headers
+
+# DELETE
+# response returns empty JSON objet
