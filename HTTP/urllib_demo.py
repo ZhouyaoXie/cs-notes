26a60bf5975d3fb98e3977ebb0d1312dac74f142
Zhouyao Xie <zx2226@columbia.edu>
2021-10-18 09:21:16 -0400

    added a demo of GET request with headers

--- HTTP/urllib_demo.py
+++ HTTP/urllib_demo.py
@@ -2,6 +2,21 @@ import urllib.request
 import urllib.parse
 import ssl
 
+# demo for GET request with header
+import urllib
+import requests
+import json
+
+url = 'http://dummy.restapiexample.com/api/v1/employees'
+header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'}
+req = urllib.request.Request(url, headers = header)
+with urllib.request.urlopen(req) as resp:
+	file = resp.read()
+data = json.loads(file)
+print(type(data)) # dict
+print(data['data'])
+
+
 # demo for POST request
 url = 'https://nature.com/'
 values = {'q':'music', 'journal':'nature', 'order':'relevance'}
@@ -18,6 +33,7 @@ resp = urllib.request.urlopen(req, context = ssl_context)
 print(resp.readline())
 print(resp.readline())
 print(resp.readline())
+
 
 # example of urllib.parse.urlparse
 o = urllib.parse.urlparse('http://www.cwi.nl:80/%7Eguido/Python.html')
